4300fbc29732033c595decddc44fbe016d63d2ea
Nicena Fontaine <nicenas.email@gmail.com>
2019-01-10 12:05:29 -0500

    Move image src to global variables and adjust showImage to access this.

--- fun-with-lolcat-button/app.js
+++ fun-with-lolcat-button/app.js
@@ -2,11 +2,12 @@
 var button = document.getElementById('lolcatButton');
 var popupImage = document.getElementById('lolcatImage');
 var reveal = document.getElementById('bigReveal');
+var image = "https://upload.wikimedia.org/wikipedia/commons/a/ab/Lolcat_in_folder.jpg";
 
 //function to reveal image
 var showImage = function () {
-	reveal.style.display = "block";//used this to test the that event handler was working properly, since I was struggling with the steps for the function.
-	popupImage.src = "https://upload.wikimedia.org/wikipedia/commons/a/ab/Lolcat_in_folder.jpg";
+	reveal.style.display = "block";
+	popupImage.src = image;
 };
 //event handler = on click, run showImage()
 button.addEventListener('click', showImage);
